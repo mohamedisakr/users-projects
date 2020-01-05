79ba5afc530be0fa6c11edcde7810c7c5d04fefd
mohamedisakr <mohamedisakr@gmail.com>
2020-01-05 13:04:37 +0200

    Updates

---
+++ precache-manifest.0f505a8ab1e5bde4c5133b9066b2acd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2112b9598fe454c95b6cb767b0d8f38b",
+    "url": "/users-projects/index.html"
+  },
+  {
+    "revision": "69d24a80b18e8ded8294",
+    "url": "/users-projects/static/css/main.b0296d99.chunk.css"
+  },
+  {
+    "revision": "7db2edfa3497a19785e2",
+    "url": "/users-projects/static/js/2.c20f5734.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/users-projects/static/js/2.c20f5734.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69d24a80b18e8ded8294",
+    "url": "/users-projects/static/js/main.63ded126.chunk.js"
+  },
+  {
+    "revision": "963daf390487e4d00b7e",
+    "url": "/users-projects/static/js/runtime-main.b9b87254.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/users-projects/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/users-projects/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/users-projects/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/users-projects/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/users-projects/static/media/search.0fc053ba.svg"
+  }
+]);
